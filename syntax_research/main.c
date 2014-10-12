f27615e6afaf1818c8a4b5f84182f5cba12e1c90
VSokolskaya <victoria.sokolskaya@gmail.com>
2014-10-12 11:40:42 +0300

    Initial Commit

---
+++ syntax_research/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  syntax_research
+//
+//  Created by User Dev on 10/12/14.
+//  Copyright (c) 2014 User Dev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
